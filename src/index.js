5f70803f57e1083fbfa47d8f1d3d9870ee5564bc
Hanna <hannashishko@gmail.com>
2018-02-27 06:39:03 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+  
+  let array = equation.split(' ');
+
+    let a = parseInt(array[0]);
+    let b = parseInt(array[3] + array[4]);
+    let c = parseInt(array[7] + array[8]);
+
+ let d = b*b-4*a*c;
+
+ let x1 = Math.round((-b + Math.sqrt(d)) / (2*a));
+ let x2 = Math.round((-b - Math.sqrt(d)) / (2*a));
+  
+  return x1 < x2 ? [x1, x2] : [x2, x1];
 }
